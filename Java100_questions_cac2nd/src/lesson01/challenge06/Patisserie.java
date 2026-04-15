027d7dac552ff2bcda06783275a2a71df98fccf7
kogure <kogure@KOGURE-AEROG11.cac-k.jp.local>
2026-04-15 11:41:53 +0900

    tyare06

--- Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
@@ -21,6 +21,60 @@
 
 package lesson01.challenge06;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Patisserie {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("たいへんお待たせしました。");
+		System.out.println("【ポエール・ネルメ】");
+		System.out.println("ただいまより開店です！！");
+
+		int sitoro = 30;
+		int shoko = 30;
+		int pisu = 30;
+
+		System.out.println("シトロン      \\250 ・・・ 残り" + sitoro + "個");
+		System.out.println("ショコラ      \\280 ・・・ 残り" + shoko + "個");
+		System.out.println("ピスターシュ  \\320 ・・・ 残り" + pisu + "個");
+
+		System.out.println("それぞれ何個ずつ買いますか？（最大30個まで）");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("シトロン＞");
+		String hmsitoro = reader.readLine();
+
+		System.out.print("ショコラ＞");
+		String hmshokora = reader.readLine();
+
+		System.out.print("ピスターシュ＞");
+		String hmpisu = reader.readLine();
+
+		int sitoroko = Integer.parseInt(hmsitoro);
+		int shokorako = Integer.parseInt(hmshokora);
+		int pisuko = Integer.parseInt(hmpisu);
+
+		int sitozaiko = (30 - sitoroko);
+		int shokozaiko = (30 - shokorako);
+		int pisuzaiko = (30 - pisuko);
+
+		int totalcount = sitoroko + shokorako + pisuko;
+
+		int totalmoney = (sitoroko * 250) + (shokorako * 280) + (pisuko * 320);
+
+		System.out.println("シトロン" + hmsitoro + "個・・・残り" + sitozaiko + "個");
+		System.out.println("ショコラ" + hmshokora + "個・・・残り" + shokozaiko + "個");
+		System.out.println("ピスターシュ" + hmpisu + "個・・・残り" + pisuzaiko + "個");
+
+		System.out.println("合計個数" + totalcount + "個");
+		System.out.println("合計金額" + totalmoney + "円");
+		System.out.println("をお買い上げですね。");
+		System.out.println("承りました！");
+
+	}
 
 }
